e6b171539706cc84d41c267b2d87a044ee985591
Josiah Maina <josiah5115@gmail.com>
2018-11-28 02:20:19 -0700

    Buttons & Input Fields Styles & Variables

--- app/index.js
+++ app/index.js
@@ -4,8 +4,9 @@ import Home from './screens/Home';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
-
-  $white: '#FFFFFF'
+  $white: '#FFFFFF',
+  $border: '#E2E2E2',
+  $inputText: '#797979',
 });
 
 export default () => <Home />;
