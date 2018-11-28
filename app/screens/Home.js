3e16e1cf8e1f9fcbc8930528c1d11da86a86b895
Josiah Maina <josiah5115@gmail.com>
2018-11-27 21:38:33 -0700

    Render TextInput Comp in Home.js

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
 
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
+import { InputWithButton } from '../components/TextInput'
 
-export default () => (
- <Container>
-  <StatusBar translucent={false} barStyle="light-content" />
-  <Logo />
- </Container>
-);
+export default class Home extends Component {
+ render() {
+  return (
+    <Container>
+     <StatusBar translucent={false} barStyle="light-content" />
+     <Logo />
+     <InputWithButton 
+      // buttonText={}
+      // onPress={}
+     />
+     <InputWithButton 
+      // buttonText={}
+      // onPress={}
+      editable={false}
+     />
+    </Container>
+  );
+ }
+}
