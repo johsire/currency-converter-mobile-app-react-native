60ea0378fd19df7d0870d00f0c0ff1f5d5876f40
Josiah Maina <josiah5115@gmail.com>
2018-11-27 21:22:03 -0700

    TextInput/InputW/Buttn Comp Init

---
+++ app/components/TextInput/InputWithButton.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, TouchableHighlight, TextInput } from 'react-native';
+
+import styles from './styles';
+
+const InputWithButton = () => null;
+
+export default InputWithButton;
