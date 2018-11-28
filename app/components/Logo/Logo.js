897dd3bc4cd3ae6425ab1c99c0c7f857ee9191c2
Josiah Maina <josiah5115@gmail.com>
2018-11-27 18:54:09 -0700

    Logo Comp & Images Setup

---
+++ app/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+const Logo = () => (
+ <View>
+  <Image>
+   <Image />
+  </Image>
+  <Text>Curreny Converter</Text>
+ </View>
+);
+
+export default Logo;
