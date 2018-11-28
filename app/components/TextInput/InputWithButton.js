04cba23d9d399f1033a558d0a412d8ba1f74bc53
Josiah Maina <josiah5115@gmail.com>
2018-11-28 01:43:59 -0700

    onPress & buttonText Fn hooked up to InputW/Butn.js

--- app/components/TextInput/InputWithButton.js
+++ app/components/TextInput/InputWithButton.js
@@ -4,6 +4,20 @@ import { View, Text, TouchableHighlight, TextInput } from 'react-native';
 
 import styles from './styles';
 
-const InputWithButton = () => null;
+const InputWithButton = ({ onPress, buttonText, editable = true }) => (
+ <View style={styles.container}>
+  <TouchableHighlight style={styles.buttonContainer} onPress={onPress}>
+   <Text style={styles.buttonText}>{buttonText}</Text>
+  </TouchableHighlight>
+  <View style={styles.border} />
+  <TextInput style={styles.input} />
+ </View>
+);
+
+InputWithButton.propTypes = {
+ onPress: PropTypes.func,
+ buttonText: PropTypes.string,
+ editable: PropTypes.bool,
+};
 
 export default InputWithButton;
