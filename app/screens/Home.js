9047d011f0f3ba2405710cd7cbf5c43e617f9f89
Josiah Maina <josiah5115@gmail.com>
2018-11-28 01:29:52 -0700

    buttonTex & onPress Fn Setup

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -3,24 +3,37 @@ import { StatusBar } from 'react-native';
 
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
-import { InputWithButton } from '../components/TextInput'
+import { InputWithButton } from '../components/TextInput';
+
+const TEMP_BASE_CURRENCY = 'USD';
+const TEMP_QUOTE_CURRENCY = 'GBP';
+const TEMP_BASE_PRICE = '100';
+const TEMP_QUOTE_PRICE = '79.74';
 
 export default class Home extends Component {
+  handlePressBaseCurrency = () => {
+    console.log('press base');
+  };
+  
+  handlePressQuoteCurrency = () => {
+    console.log('press quote');
+  };
+  
  render() {
   return (
     <Container>
      <StatusBar translucent={false} barStyle="light-content" />
      <Logo />
-     <InputWithButton 
-      // buttonText={}
-      // onPress={}
+     <InputWithButton
+        buttonText={TEMP_BASE_CURRENCY}
+        onPress={this.handlePressBaseCurrency}
      />
-     <InputWithButton 
-      // buttonText={}
-      // onPress={}
-      editable={false}
+     <InputWithButton
+        buttonText={TEMP_QUOTE_CURRENCY}
+        onPress={this.handlePressQuoteCurrency}
+        editable={false}
      />
     </Container>
   );
- }
-}
+ };
+};
