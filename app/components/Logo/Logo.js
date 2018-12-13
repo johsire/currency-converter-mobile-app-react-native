81c1e0c765d4a37e6c03f3676785d77043ad8ecc
Josiah Maina <josiah5115@gmail.com>
2018-12-12 20:35:13 -0700

    keyboard add-Listeners

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,18 +1,40 @@
 // Dependencies Imports----------->>>>>
-import React from 'react';
-import { View, Text, ImageBackground, Image } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, ImageBackground, Image, Keyboard } from 'react-native';
 
 // Styles Imports------------------>>>>
 import styles from './styles';
 
 // Logo Component-------------------->>>>
-const Logo = () => (
- <View style={styles.container}>
-  <ImageBackground resizeMode="contain" style={styles.containerImage} source={require('./images/background.png')} >
-   <Image resizeMode="contain" style={styles.image} source={require('./images/logo.png')} />
-  </ImageBackground>
-  <Text style={styles.text}>Curreny Converter</Text>
- </View>
-);
+class Logo extends Component {
+ componentDidMount() {
+  this.keyboardShowListener = keyboard.addListener('keyboardWillShow', this.keyboardShow);
+  this.keyboardHideListener = keyboard.addListener('keyboardWillHide', this.keyboardHide);
+ };
+
+ componentWillUnmount() {
+  this.keyboardShowListener.remove();
+  this.keyboardHideListener.remove();
+ };
+
+ keyboardShow = () { 
+  console.log('keyboard did show');
+  };
+
+ keyboardHide = () { 
+  console.log('keyboard did hide');
+  };
+
+ render() {
+  return (
+   <View style={styles.container}>
+    <ImageBackground resizeMode="contain" style={styles.containerImage} source={require('./images/background.png')} >
+     <Image resizeMode="contain" style={styles.image} source={require('./images/logo.png')} />
+    </ImageBackground>
+    <Text style={styles.text}>Curreny Converter</Text>
+   </View>
+  );
+ }
+};
 
 export default Logo;
