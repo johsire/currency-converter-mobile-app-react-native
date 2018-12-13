7199bcb4fce61bd025d4ace61b280cc54d1bb88a
Josiah Maina <josiah5115@gmail.com>
2018-12-13 00:45:31 -0700

    Animation

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,12 +1,19 @@
 // Dependencies Imports----------->>>>>
 import React, { Component } from 'react';
-import { View, Text, ImageBackground, Image, Keyboard } from 'react-native';
+import { View, Text, ImageBackground, Image, Keyboard, Animated } from 'react-native';
 
 // Styles Imports------------------>>>>
 import styles from './styles';
 
+const ANIMATION_DURATION = 250;
+
 // Logo Component-------------------->>>>
 class Logo extends Component {
+ constructor(props) {
+  super(props);
+
+  this.containerImageWidth = new Animated.Value(styles.$largeImageSize);
+ }
  componentDidMount() {
   this.keyboardShowListener = keyboard.addListener('keyboardWillShow', this.keyboardShow);
   this.keyboardHideListener = keyboard.addListener('keyboardWillHide', this.keyboardHide);
@@ -17,20 +24,38 @@ class Logo extends Component {
   this.keyboardHideListener.remove();
  };
 
- keyboardShow = () { 
-  console.log('keyboard did show');
+ keyboardShow = () => { 
+  Animated.timing(this.containerImageWidth, {
+   toValue: styles.$smallContainerSize,
+   duration: ANIMATION_DURATION,
+  }).start();
   };
 
- keyboardHide = () { 
+ keyboardHide = () => { 
   console.log('keyboard did hide');
   };
 
  render() {
+  const containerImageStyle = [
+   styles.contanerImage,
+   { width: this.containerImageWidth, height: this.containerImageWidth },
+  ];
+
+  const imageStyle = [
+   styles.logo,
+   { width: this.imageWidth },
+  ];
+
   return (
    <View style={styles.container}>
-    <ImageBackground resizeMode="contain" style={styles.containerImage} source={require('./images/background.png')} >
-     <Image resizeMode="contain" style={styles.image} source={require('./images/logo.png')} />
-    </ImageBackground>
+    <Animated.ImageBackground resizeMode="contain" style={containerImageStyle} source={require('./images/background.png')}
+    >
+     <Animated.ImageBackground resizeMode="contain"
+     style={imageStyle}
+      source={require('./images/logo.png')}
+      
+     />
+    </Animated.ImageBackground>
     <Text style={styles.text}>Curreny Converter</Text>
    </View>
   );
