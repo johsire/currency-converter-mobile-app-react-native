f5295c615a98d66339397bfcb237666f441541c9
Josiah Maina <josiah5115@gmail.com>
2018-12-20 23:23:22 -0700

    Currency List- Flat-List

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
@@ -11,4 +11,4 @@ EStyleSheet.build({
   $inputText: '#797979',
 });
 
-export default () => <Home />;
+export default () => <CurrencyList />;
