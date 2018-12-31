ea416e6d1ff9f9118bf0c9d0e439c0851680f939
dev_johsire <josiah5115@gmail.com>
2018-12-31 14:51:07 -0700

    Redux Setup- Currency Reducers

---
+++ app/reducers/currencies.js
@@ -0,0 +1,33 @@
+
+/* eslint-disable no-console */
+
+import { CHANGE_CURRENCY_AMOUNT, SWAP_CURRENCY } from '../actions/currencies';
+
+const initialState = {
+  baseCurrency: 'USD',
+  quoteCurrency: 'GBP',
+  amount: 100,
+  conversion: {},
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CHANGE_CURRENCY_AMOUNT:
+      return {
+        ...state,
+        amount: action.amount || 0,
+      };
+
+    case SWAP_CURRENCY:
+      return {
+        ...state,
+        baseCurrency: state.quoteCurrency,
+        quoteCurrency: state.baseCurrency,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
---
+++ app/reducers/index.js
@@ -0,0 +1,8 @@
+
+import { combinedReducers } from 'redux';
+
+import currencies from './currencies';
+
+export default combinedReducers({
+  currencies,
+});
