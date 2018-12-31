90ebf6e6feaf19eb96250c9c6fdf4afb4562cb0a
dev_johsire <josiah5115@gmail.com>
2018-12-31 01:59:49 -0700

    Alert Provider & Connect Alert Code

--- app/components/Alert/index.js
+++ app/components/Alert/index.js
@@ -1,1 +1,7 @@
 
+// https://github.com/testshallpass/react-native-dropdownalert/issues/30
+
+import AlertProvider from './AlertProvider';
+import connectAlert from './connectAlert';
+
+export { AlertProvider, connectAlert };
